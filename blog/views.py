c4c66cc7bb3733d9c43485766d9de4fb6af34659
Akbarjon Xatamov <akosh3666@gmail.com>
2021-05-05 09:27:48 +0500

    Linkedin

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 # Stranitsa otdelnogo bloga
+from .models import Post
 
 # Create your views here.
+def showblog(request):                          #
+    posts = Post.objects
+    return render(request,'blog/blog.html' , {'posts': posts})     # Stranisa bloga\
+
+def specific_post(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/specific_post.html', {'post':post})
--- events/views.py
+++ events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Event  # EVENT lesson
 
 # Create your views here.
+events = Event.objects       # EVENT lesson
+def home(request):
+    return render(request,'events/home.html', {'events': events})
